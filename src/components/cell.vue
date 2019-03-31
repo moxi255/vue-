826043dab8b2dc33ba5f9ed811ee661f32bfed0e
moxi255 <614303219@qq.com>
2019-03-31 16:51:25 +0800

    first commit

---
+++ src/components/cell.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'cell',
+  data () {
+    return {
+      msg: '多页面测试'
+    }
+  }
+}
+</script>
+
+<style scoped></style>
